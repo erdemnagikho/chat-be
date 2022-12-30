3382e59a7ceed70c1f3cf54b79e5f4f151a1dce5
Erdem Nayir <erdemnayir@Erdems-MacBook-Pro.local>
2022-12-30 14:37:24 +0300

    Auth feature

---
+++ src/shared/services/queues/user.queue.ts
@@ -0,0 +1,16 @@
+import { IAuthJob } from '@auth/interfaces/auth.interface';
+import { BaseQueue } from '@service/queues/base.queue';
+import { userWorker } from '@worker/user.worker';
+
+class UserQueue extends BaseQueue {
+  constructor() {
+    super('auth');
+    this.processJob('addUserToDB', 5, userWorker.addUserToDB);
+  }
+
+  public addUserJob(name: string, data: IAuthJob): void {
+    this.addJob(name, data);
+  }
+}
+
+export const userQueue: UserQueue = new UserQueue();
